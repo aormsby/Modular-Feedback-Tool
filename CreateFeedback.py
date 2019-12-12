0997d61860400b17ce83663247884a28c1eb99ff
Adam Ormsby <ormsbyadam@gmail.com>
2019-12-12 09:20:30 +0700

    improved var names, added proper function comments, added Run() to consolidate functions at top

--- CreateFeedback.py
+++ CreateFeedback.py
@@ -1,49 +1,55 @@
 import sys
 
-# TODO make vars more generic
-# TODO split paragraph building and gender replacements
-
 rubricData = []
-classData = []
+groupInputData = []
 
-# get rubric file data
+
+def Run():
+    ReadFiles()
+    CompileComments()
+    # print(rubricData)
+    # print(groupInputData)
+
+
 def ReadFiles():
+    """get rubric/group file data"""
 
     with open(sys.argv[1]) as rubric:
-    # with open("Rubric8.csv") as rubric:
+        # with open("Samples/SampleRubric.csv") as rubric:
         readin = rubric.readlines()
         global rubricData
         for line in readin:
             rubricData.append(line.strip("\n").split("|"))
 
     with open(sys.argv[2]) as classList:
-    # with open("TestClass.csv") as classList:
+        # with open("Samples/SampleInput.csv") as groupInputData:
         readin = classList.readlines()
-        global classData
+        global groupInputData
         for line in readin:
-            classData.append(line.strip("\n").split(","))
+            groupInputData.append(line.strip("\n").split(","))
 
 
-def BuildParagraph(student):
-    paragraph = student[0] + "\n" + student[1]
+def BuildParagraph(individual):
+    """construct paragraph using individual data to pull pieces from rubric"""
+    paragraph = individual[0] + "\n" + individual[1]
 
     for i in range(0, len(rubricData)):
-        # print(rubricData[i][int(student[i+3])])
-        paragraph += " " + rubricData[i][int(student[i+3])]
-    
-    if (student[2] == "f"):
+        # print(rubricData[i][int(individual[i+3])])
+        paragraph += " " + rubricData[i][int(individual[i+3])]
+
+    if (individual[2] == "f"):
         paragraph = paragraph.replace("[Pro]", "She")
         paragraph = paragraph.replace("[pro]", "she")
         paragraph = paragraph.replace("[Pos]", "Her")
         paragraph = paragraph.replace("[pos]", "her")
         paragraph = paragraph.replace("[obj]", "her")
-    elif (student[2] == "m"):
+    elif (individual[2] == "m"):
         paragraph = paragraph.replace("[Pro]", "He")
         paragraph = paragraph.replace("[pro]", "he")
         paragraph = paragraph.replace("[Pos]", "His")
         paragraph = paragraph.replace("[pos]", "his")
         paragraph = paragraph.replace("[obj]", "him")
-    elif (student[2] == "t"):
+    elif (individual[2] == "t"):
         paragraph = paragraph.replace("[Pro]", "They")
         paragraph = paragraph.replace("[pro]", "they")
         paragraph = paragraph.replace("[Pos]", "Their")
@@ -51,25 +57,23 @@ def BuildParagraph(student):
         paragraph = paragraph.replace("[obj]", "them")
     else:
         paragraph = "OOPS."
-    
+
     return paragraph
-    
 
-# read student comment generation data --> Nguyen Ngoc Linh,Linh,F,1,3,2,2,1,2
-# then output the generated comment to a new file
-def GenerateComments():
 
-    with open(classData[0][0] + "Comments.txt", "w") as output:
-        output.write(classData[0][0] + "\n")
+def CompileComments():
+    """read individual comment generation data --> Frodo Baggins,Frodo,m,1,1,1,1,1
+    then output the generated comment to a new file (currently overwrites)"""
+
+    with open(groupInputData[0][0] + "Comments.txt", "w") as output:
+        output.write(groupInputData[0][0] + "\n")
         output.write("\n")
 
-        for i in range(1, len(classData)):
-            studentComment = BuildParagraph(classData[i])
-            
-            output.write(studentComment + "\n")
+        for i in range(1, len(groupInputData)):
+            individualComment = BuildParagraph(groupInputData[i])
+
+            output.write(individualComment + "\n")
             output.write("\n")
 
-ReadFiles()
-GenerateComments()
-# print(rubricData)
-# print(classData)
+
+Run()
