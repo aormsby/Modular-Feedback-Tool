a72cb3f031850cf0f590c02596cd24c72cf29154
Adam Ormsby <ormsbyadam@gmail.com>
2019-12-18 08:57:56 +0700

    add output overwrite check

--- CreateFeedback.py
+++ CreateFeedback.py
@@ -1,5 +1,6 @@
 import sys
 from GenderSwap import InsertGenderLanguage
+from pathlib import Path
 
 rubricData = []
 groupInputData = []
@@ -15,16 +16,16 @@ def Run():
 def ReadFiles():
     """get rubric/group file data"""
 
-    with open(sys.argv[1]) as rubric:
-        # with open("Samples/SampleRubric.csv") as rubric:
+    # with open(sys.argv[1]) as rubric:
+    with open("Samples/SampleRubric.csv") as rubric:
         readin = rubric.readlines()
         global rubricData
         for line in readin:
             rubricData.append(line.strip("\n").split("|"))
 
-    with open(sys.argv[2]) as classList:
-        # with open("Samples/SampleInput.csv") as groupInputData:
-        readin = classList.readlines()
+    # with open(sys.argv[2]) as groupList:
+    with open("Samples/SampleInput.csv") as groupList:
+        readin = groupList.readlines()
         global groupInputData
         for line in readin:
             groupInputData.append(line.strip("\n").split(","))
@@ -37,16 +38,15 @@ def BuildParagraph(individual):
     for i in range(0, len(rubricData)):
         # print(rubricData[i][int(individual[i+3])])
         paragraph += " " + rubricData[i][int(individual[i+3])]
-    
+
     paragraph = InsertGenderLanguage(paragraph, individual[2])
     return paragraph
 
 
-def CompileComments():
-    """read individual comment generation data --> Frodo Baggins,Frodo,m,1,1,1,1,1
-    then output the generated comment to a new file (currently overwrites)"""
+def OutputToPath(filePath):
 
-    with open(groupInputData[0][0] + "Comments.txt", "w") as output:
+    # with open(groupInputData[0][0] + "Comments.txt", "w") as output:
+    with open(filePath, "w") as output:
         output.write(groupInputData[0][0] + "\n")
         output.write("\n")
 
@@ -55,6 +55,30 @@ def CompileComments():
 
             output.write(individualComment + "\n")
             output.write("\n")
+
+
+def CompileComments():
+    """read individual comment generation data --> Frodo Baggins,Frodo,m,1,1,1,1,1
+    then output the generated comment to a new file (currently overwrites)"""
+
+    # check if output file exists, ask for overwrite
+    writePath = groupInputData[0][0] + "Comments.txt"
+    if Path(writePath).exists():
+        if input("Output file " + writePath + " already exists. Overwrite file? [y/n]: ") == "n":
+            i = 1
+            while True:
+                if not Path(writePath.replace(".", "("+str(i)+").")).exists():
+                    writePath = writePath.replace(".", "("+str(i)+").")
+                    break
+                i += 1
+
+            OutputToPath(writePath)
+            print("Process completed - new file " + writePath + " created")
+        else:
+            OutputToPath(writePath)
+            print("Process completed - " + writePath + " overwritten")
+    else:
+        OutputToPath(writePath)
 
 
 Run()
